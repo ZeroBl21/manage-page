21f6581adad8cde5c2a831722373c4183a898672
Darwin Perdomo <ZeroBl@hotmail.com>
2022-05-02 17:39:29 -0400

    refactor(constants): Users' images and commentaries have been moved to their own file

---
+++ src/constants/users.js
@@ -0,0 +1,37 @@
+import ali from "../assets/avatar-ali.png";
+import anisha from "../assets/avatar-anisha.png";
+import richard from "../assets/avatar-richard.png";
+import shanai from "../assets/avatar-shanai.png";
+
+const Users = [
+  {
+    id: 12345,
+    image: anisha,
+    name: "Anisha Li",
+    commentary: `"Manage has supercharged our team's workflow. The ability to maintain visibility
+    on larger milestones at all times keeps everyone motivated."`,
+  },
+  {
+    id: 23456,
+    image: ali,
+    name: "Ali Bravo",
+    commentary: `"We have been able to cancel so many other subscriptions since using Manage.
+    There is no more cross-channel confusion and everyone is much more focused."`,
+  },
+  {
+    id: 34567,
+    image: richard,
+    name: "Richard Watts",
+    commentary: `"Manage allows us to provide structure and process. It keeps us organized and
+    focused. I can't stop recommending them to everyone I talk to!"`,
+  },
+  {
+    id: 45678,
+    image: shanai,
+    name: "Shadai Gough",
+    commentary: `"Their software allows us to track, magage and collaborate on our projects from
+    anywhere. It keeps the whole team in-sync without being intrusive."`,
+  },
+];
+
+export default Users;
