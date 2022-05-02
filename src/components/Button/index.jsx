6c89768454b66e9eb22240bd5a6d0a0a1b730225
Darwin Perdomo <ZeroBl@hotmail.com>
2022-05-01 23:13:39 -0400

    feat(Button/App): Added inverted color styling and fix App color variable error

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
-import './Button.css';
+import "./Button.css";
 
-const Button = ({ text, handleClick, hide }) => {
-
+const Button = ({ text, handleClick, hide, inverted }) => {
   return (
-    <button className={`custom__button ${ hide ? "hide" : "" }`} onClick={handleClick}>{text}</button>
-  )
-}
+    <button
+      className={`
+        custom__button 
+        ${hide ? "hide" : ""}
+        ${inverted ? "alt" : ""}
+      `}
+      onClick={handleClick}
+    >
+      {text}
+    </button>
+  );
+};
 
 export default Button;
